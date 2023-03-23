fb81edb7b0f51e393a3457b4623578e5d374b8cc
Easton <easton.fritsch21@gmail.com>
2023-03-23 12:27:31 -0600

    Finished project with all passing but did not do stretches on task 2

--- index.js
+++ index.js
@@ -15,10 +15,32 @@
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name, age) {
+    this.name = name;
+    this.age = age;
+    this.stomach = [];
 }
 
+Person.prototype.eat = function(edible){
+    if(this.stomach.length < 10){
+      this.stomach.push(edible);
+    } 
+}
+
+Person.prototype.poop = function(){
+    this.stomach = [];
+}
+
+Person.prototype.toString = function(){
+    return (`${this.name}, ${this.age}`);
+}
+
+const easton = new Person('Easton', 23);
+easton.eat('pizza');
+easton.eat('dr pepper')
+
+console.log('task 1a:', easton.toString());
+console.log('task 1b:', easton.stomach);
 
 /*
   TASK 2
@@ -36,10 +58,26 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model, milesPerGallon){
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
 }
 
+Car.prototype.fill = function(gallons){
+   this.tank = this.tank + gallons;
+   return this.tank;
+}
+
+Car.prototype.drive = function(distance){
+  return (`Odometer: ${this.odometer + distance}, Tank: ${this.tank - this.milesPerGallon}`);
+}
+
+const mustang = new Car("Mustang", 21)
+
+console.log('task 2a:', mustang.fill(4));
+console.log('task 2b:', mustang.milesPerGallon);
 
 /*
   TASK 3
@@ -49,18 +87,29 @@ function Car() {
         + Should return a string "Playing with x", x being the favorite toy.
 */
 
-function Baby() {
-
+function Baby(name, age, favoriteToy) {
+      this.name = name;
+      this.age = age;
+      this.favoriteToy = favoriteToy;
 }
 
+Baby.prototype = Object.create(Person.prototype);
+
+Baby.prototype.play = function(){
+  return (`Playing with ${this.favoriteToy}.`);
+}
+
+const gavin = new Baby('Gavin', 2, 'Poop Plushie');
+
+console.log('task 3:', gavin.play());
 
 /* 
   TASK 4
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. When using this keyword in the Global/Window scope, it will be in the window/ console object
+  2. Implicit Binding is whenever a preceding dot calls a function, the object before the dot is "this"
+  3. New binding is when using a constructor it refers to the specfic instance of the object created by the constructor
+  4. Explicit binding is whenever we use JS call or apply methods
 */
 
 ///////// END OF CHALLENGE /////////
